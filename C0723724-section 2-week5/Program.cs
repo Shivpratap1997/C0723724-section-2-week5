f8db54c861e2a659af1db79f42bed39f2a7c6b59
visualstudio <visualstudio@122-PC15>
2019-02-06 09:53:54 -0500

    HELLO

---
+++ C0723724-section 2-week5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0723724_section_2_week5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
